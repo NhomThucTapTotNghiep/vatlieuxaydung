513e65f967a34d6fb3f071109a7190dfcf20dab4
khanh2091 <94159921+khanh2091@users.noreply.github.com>
2022-04-23 09:47:10 +0700

    check point

--- admin/dashboard/layout/head.php
+++ admin/dashboard/layout/head.php
@@ -39,9 +39,9 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
 
-    <!-- <link rel="stylesheet" type="text/css"
-        href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css"> -->
-    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css"> -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
 
 
 
